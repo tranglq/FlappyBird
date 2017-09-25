63f1cdc079569851f3c6e7273750f14688fbd168
tranglq <lequynhtrang120296@gmail.com>
2017-09-25 16:19:51 +0700

    setting up player

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour {
+    public float yMin, yMax;
+    public float speed;
+
+    void Update () {
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(0.0f, moveVertical);
+
+        GetComponent<Rigidbody2D>().velocity = movement * speed; // GetComponent<Rigidbody2D>().drag;
+
+        GetComponent<Rigidbody2D>().position = new Vector2
+        (
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody2D>().position.y, yMin, yMax)
+        );
+	}
+}
